a2346995f6a355ca188f919a84ba5bdbd353a020
Greensky <david.hesliere@gmail.com>
2022-12-04 12:58:58 +0100

    chore: :hammer: Add resizeString()

--- src/assets/functions.ts
+++ src/assets/functions.ts
@@ -7,3 +7,7 @@ export const systRep = (interaction: CommandInteraction, reply: InteractionReply
 export const userName = (user: User) => `<@${user.id}> ( ${user.tag} \`${user.id}\` )`;
 export const dateNow = () => `<t:${(Date.now() / 1000).toFixed(0)}:F> ( <t:${(Date.now() / 1000).toFixed(0)}:R> )`;
 export const capitalize = (str: string) => str[0].toUpperCase() + str.slice(1);
+export const resizeString = ({ str, maxLength = 1000 }: { str: string; maxLength?: number }) => {
+    if (str.length <= maxLength) return str;
+    return str.substring(0, maxLength - 3) + '...';
+}
